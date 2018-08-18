3523e2de04717c6c6d3e655b1e3ce0752e1755db
Andres <vasquez.andres2@gmail.com>
2018-08-18 16:34:01 -0600

    headerhalf

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Viral</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="C:\xampp\htdocs\viral\wp-content\themes\viral-tema\assets\css\bootstrap.min.css">
+    <link rel="stylesheet" href="C:\xampp\htdocs\viral\wp-content\themes\viral-tema\assets\css\all.css">
     <link rel="stylesheet" href="syle.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
 </head>
 <style>
     .container{
@@ -31,62 +33,79 @@
 <body>
     <header class="row">
         <section class="col-6 header__main">
-                <section class="col-12 header__logo"> 
-                    <figure>
-                    <img src="img/logo.png" class="img-fluid" alt="Viral Logo"/>
-                    </figure>
-                </section>
-                <nav class="col-12 header__nav"> 
-                    <ul class="nav nav__main">
-                        <li class="nav-item nav__item">
-                            <a class="nav-link active nav__link" href="#">Tecnología</a>
-                        </li>
-                        <li class="nav-item nav__item">
-                            <a class="nav-link active nav__link" href="#">YouTube</a>
-                        </li>
-                        <li class="nav-item nav__item">
-                            <a class="nav-link active nav__link" href="#">Música</a>
-                        </li>
-                        <li class="nav-item nav__item">
-                            <a class="nav-link active nav__link" href="#">Cine</a>
-                        </li>
-                        <li class="nav-item nav__item">
-                            <a class="nav-link active nav__link" href="#">Series</a>
-                        </li>
-                    </ul>
-                </nav>
-                <section class="col-6 header__sec">
-                    </div>
-                    <i class="fab fa-facebook"></i><a href="https://facebook.com"></a>
-                    <i class="fab fa-instagram"></i><a href="https://instagram.com"></a>
-                    <i class="fab fa-twitter-square"></i><a href="https://twitter"></a>
-                    <i class="fab fa-youtube-square"></i><a href="https://youtube.com"></a>
-                <section class="col-6 header__">
-                <button type="button" class="btn btn-outline-dark">Buscar aqui</button>
-                <i class="fas fa-search"></i>
-                </section>
-                <section class="w-100"></div>
-                <section class="header col-6">
-                </section>
+            <section class="col-12 header__logo"> 
+                <figure>
+                <img src="img\logo.png" class="img-fluid" alt="Viral Logo"/>
+                </figure>
+            </section>
+            <nav class="col-12 header__nav"> 
+                <ul class="nav nav__main">
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">Tecnología</a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">YouTube</a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">Música</a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">Cine</a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">Series</a>
+                    </li>
+                </ul>
+            </nav>
         </section>
-        </header>
-    <div class="container">
+        <section class="col-6 header__sec">
+            <nav class="col-5 nav_social">
+                <ul class="nav nav__main">
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">
+                        <i class="fab fa-facebook"></i><a href="https://facebook.com"></a>
+                        </a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">
+                        <i class="fab fa-instagram"></i><a href="https://instagram.com"></a>
+                        </a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">
+                        <i class="fab fa-twitter-square"></i><a href="https://twitter"></a>
+                        </a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">
+                        <i class="fab fa-youtube-square"></i><a href="https://youtube.com"></a>
+
+                        </a>
+                    </li>
+                    <li class="nav-item nav__item">
+                        <a class="nav-link active nav__link" href="#">
+                        
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+    </header>
+    <div class="container-fluid">
         <div class="row"> 
             <div class="principal col-8">
-                
             </div>
             <div class="principal col-8">
-                Tecnología
+            Tecnología
             </div>
             <div class="sidebar col-4">
-                Publicidad
-        </div>
-    </div>
-    <div class="container"> 
-        <div class="row"> 
-            <div class="footer col-12">
-            <img src="viral2.png" class="img-fluid" alt="Viral Logo 2"> 
+            Publicidad
             </div>
+            </div>
+    <div class="container-fluid"> 
+        <div class="row"> 
+        <div class="footer col-12">
+        <img src="viral2.png" class="img-fluid" alt="Viral Logo 2"> 
+        </div>
         </div>
     </div>  
 </body>
