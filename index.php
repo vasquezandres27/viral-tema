cec371caa1991e8fecc65d8069040f8c3f478efb
Andres <vasquez.andres2@gmail.com>
2018-08-18 15:33:17 -0600

    Header

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Viral</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="syle.css">
+</head>
+<style>
+    .container{
+        background-color: #FFFFFF;
+        text-align: center;
+        font-family: Tahoma;
+    } 
+    .principal{
+        background-color: #FFFFFF;
+        text-align: left;
+    } 
+    .sidebar{
+        background-color: #FFFFFF;
+    } 
+    .footer{
+        background-color: #000000;
+        text-align: center;
+        color: #FFFFFF;
+    }
+</style>
+<body>
+    <header class="row">
+        <section class="col-6 header__main">
+                <section class="col-12 header__logo"> 
+                    <figure>
+                    <img src="img/logo.png" class="img-fluid" alt="Viral Logo"/>
+                    </figure>
+                </section>
+                <nav class="col-12 header__nav"> 
+                    <ul class="nav nav__main">
+                        <li class="nav-item nav__item">
+                            <a class="nav-link active nav__link" href="#">Tecnología</a>
+                        </li>
+                        <li class="nav-item nav__item">
+                            <a class="nav-link active nav__link" href="#">YouTube</a>
+                        </li>
+                        <li class="nav-item nav__item">
+                            <a class="nav-link active nav__link" href="#">Música</a>
+                        </li>
+                        <li class="nav-item nav__item">
+                            <a class="nav-link active nav__link" href="#">Cine</a>
+                        </li>
+                        <li class="nav-item nav__item">
+                            <a class="nav-link active nav__link" href="#">Series</a>
+                        </li>
+                    </ul>
+                </nav>
+                <section class="col-6 header__sec">
+                    </div>
+                    <i class="fab fa-facebook"></i><a href="https://facebook.com"></a>
+                    <i class="fab fa-instagram"></i><a href="https://instagram.com"></a>
+                    <i class="fab fa-twitter-square"></i><a href="https://twitter"></a>
+                    <i class="fab fa-youtube-square"></i><a href="https://youtube.com"></a>
+                <section class="col-6 header__">
+                <button type="button" class="btn btn-outline-dark">Buscar aqui</button>
+                <i class="fas fa-search"></i>
+                </section>
+                <section class="w-100"></div>
+                <section class="header col-6">
+                </section>
+        </section>
+        </header>
+    <div class="container">
+        <div class="row"> 
+            <div class="principal col-8">
+                
+            </div>
+            <div class="principal col-8">
+                Tecnología
+            </div>
+            <div class="sidebar col-4">
+                Publicidad
+        </div>
+    </div>
+    <div class="container"> 
+        <div class="row"> 
+            <div class="footer col-12">
+            <img src="viral2.png" class="img-fluid" alt="Viral Logo 2"> 
+            </div>
+        </div>
+    </div>  
+</body>
+</html>
