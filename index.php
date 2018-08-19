88678c4b92489ed26a1823eb3417db44b88fb163
Andres <vasquez.andres2@gmail.com>
2018-08-18 18:48:13 -0600

    FinishedTask

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
             </nav>
            <section class="col-7 input__search">
             <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="button-addon2">
                     <div class="input-group-append">
                         <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
                     </div>
